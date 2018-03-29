9fd5b8c999b95c31a7ca1e235e17bcae5351bb01
ouba1 <31849660+ouba1@users.noreply.github.com>
2018-03-29 22:25:21 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -4,4 +4,4 @@ export PATH
 yum install unzip wget #For Centos
 apt install unzip wget #For Debian
 cd
-wget https://raw.githubusercontent.com/ouba1/ssr-2018/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
+wget https://raw.githubusercontent.com/ouba1/ssr-2018/master/ssr.zip && unzip ssr.zip && cd SSR* && bash install.sh
